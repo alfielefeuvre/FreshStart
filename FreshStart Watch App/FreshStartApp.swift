40f6daa26873f5c9a69d7f139d0e7b1311945edd
Alfie Le Feuvre <alfielefeuvre@icloud.com>
2024-04-30 20:02:03 +0100

    Initial Commit

---
+++ FreshStart Watch App/FreshStartApp.swift
@@ -0,0 +1,17 @@
+//
+//  FreshStartApp.swift
+//  FreshStart Watch App
+//
+//  Created by Alfie Le Feuvre on 30/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct FreshStart_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FreshStart/FreshStartApp.swift
@@ -0,0 +1,17 @@
+//
+//  FreshStartApp.swift
+//  FreshStart
+//
+//  Created by Alfie Le Feuvre on 30/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct FreshStartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
